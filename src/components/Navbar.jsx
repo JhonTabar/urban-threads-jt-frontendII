24f78491b5ff2545d66e0671e8c374adaa65416a
Jhonatan Tabares <tabaresjhonatan771@gmail.com>
2026-04-21 09:59:23 -0500

    Creación base projecto

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+<header class = "header">
+    <nav class="nav">
+        <div class="nav-left"> URBAN THREADS </div>
+        <div class="nav-right">
+            <ul>
+                <li> <a> Inicio </a></li>
+                <li> <a> Productos </a></li>
+                <li> <a> Contacto </a></li>
+            </ul>
+        </div>
+    </nav>    
+</header>
